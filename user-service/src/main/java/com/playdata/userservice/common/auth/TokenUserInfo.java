d6ba3ddfcb19d40bce15ce96787f7414a03ea3d6
LeeKM321 <stephen4951@gmail.com>
2025-04-29 16:37:15 +0900

    User Service 프로젝트 추가 및 정리

---
+++ user-service/src/main/java/com/playdata/userservice/common/auth/TokenUserInfo.java
@@ -0,0 +1,16 @@
+package com.playdata.userservice.common.auth;
+
+import com.playdata.userservice.user.entity.Role;
+import lombok.*;
+
+@Setter @Getter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class TokenUserInfo {
+
+    private String email;
+    private Role role;
+
+}
